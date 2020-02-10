c21c74d581b0f9cc1565ca2c4d76aec5b65e9878
Roger Philippe Pereira <rogerphilippe@outlook.com>
2020-02-10 13:06:04 -0300

    Semaphore sem thread, implementada thread na chamada do método run do semaphore na MainActivity.

--- app/src/main/java/br/com/philippesis/semaforoemetodossincronizados/MainActivity.kt
+++ app/src/main/java/br/com/philippesis/semaforoemetodossincronizados/MainActivity.kt
@@ -2,11 +2,25 @@ package br.com.philippesis.semaforoemetodossincronizados
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnStart.setOnClickListener { start() }
+    }
+
+    private fun start() {
+
+        val semBA = SemaphoreImplementationB("A")
+
+        Thread(Runnable { semBA.run() }).start()
+
+        val semBB = SemaphoreImplementationB("B")
+
+        Thread(Runnable { semBB.run() }).start()
     }
 }
